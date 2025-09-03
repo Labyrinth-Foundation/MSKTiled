7642b9968554e12181acd48f5cb39ca9be4c2d87
Blaine <blainesolomon@users.noreply.github.com>
2025-09-02 19:17:51 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,25 +1,19 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "MSKTiled",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MSKTiled",
             targets: ["MSKTiled"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.56.2")
+        // no external dependencies
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MSKTiled"//,
-//            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+            name: "MSKTiled"
         ),
         .testTarget(
             name: "MSKTiledTests",
